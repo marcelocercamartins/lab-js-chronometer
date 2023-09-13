e8dce93e35dd2e272e7a7d270de6ff145e021162
marcelocercamartins <141592873+marcelocercamartins@users.noreply.github.com>
2023-09-13 10:26:57 +0100

    Update chronometer.js

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,56 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    if (!this.intervalId) {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+        printTimeCallback(this.currentTime);
+
+        if (this.currentTime === 3) {
+          this.stop();
+        }
+      }, 1000); 
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return value < 10 ? `0${value}` : `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    this.intervalId = null;
   }
 
   reset() {
-    // ... your code goes here
+    this.stop();
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
   }
 }
+
+
+const myChronometer = new Chronometer();
+
+const printTimeCallback = (currentTime) => {
+  console.log(`Current Time: ${myChronometer.split()}`);
+};
+
+myChronometer.start(printTimeCallback);
